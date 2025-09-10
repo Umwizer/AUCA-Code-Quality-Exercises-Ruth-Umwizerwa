9748380fd4856c201725ca2c8630aaa8addbcbbc
Umwizer <ruthumwizerwa@gmail.com>
2025-09-10 12:40:28 +0200

    exercis

--- exercise2/CodeQuality1.js
+++
@@ -1,7 +0,0 @@
-function calculateAverage(numbers) {
-  if (numbers.length === 0) return 0;
-  const sum = numbers.reduce((acc, curr) => acc + curr, 0);
-  return sum / numbers.length;
-}
-const num = [10, 20, 30, 10, 50];
-console.log(calculateAverage(num));
--- exercise2/CodeQuality5.js
+++
@@ -1,6 +0,0 @@
-function calculateFactorial(n) {
-  if (n < 0) throw new Error("Factorial is not defined for negative numbers");
-  return n <= 1 ? 1 : n * calculateFactorial(n - 1);
-}
-
-console.log(calculateFactorial(5));
